0473f970e8b927bc35417225900379b75ea79f11
sbytony <zhps500126@gmail.com>
2025-06-11 22:59:30 +0800

    Finish part2

---
+++ part2/Loop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        int numbersRead = 0;
+        int sum = 0;
+
+        while (true) {
+            if (numbersRead == 5) {
+                break;
+            }
+            System.out.println("Input num");
+            sum = sum + Integer.valueOf(reader.nextLine());
+            numbersRead = numbersRead + 1;
+        }
+        System.out.println("The sum of the num is: " + sum);
+    }
+}
---
+++ part2/ProgramStructure.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ProgramStructure {
+    public static void main(String[] args) {
+        greet(1);
+        System.out.println("");
+        greet(3);
+    }
+
+    public static void greet(int numOfTime) {
+        int i = 0;
+        while (i < numOfTime) {
+            System.out.println("Greeting");
+            i++;
+        }
+    }
+}
